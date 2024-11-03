2bd25f55d9ee73953b36a91019e696f068ef40db
Risminator <vjdronov@yandex.ru>
2024-11-03 14:28:33 +0300

    feat: finished physics props

--- scripts/physics_prop.gd
+++ scripts/physics_prop.gd
@@ -1,17 +1,58 @@
 extends RigidBody2D
+class_name PhysicsProp
 
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var collision_polygon: CollisionPolygon2D = $CollisionPolygon2D
 
 @export var sprite_texture: Texture
 
-const SPEED = 50
+
+const MAX_SPEED = 1000
 var has_mouse:bool = false
 var prop_taken:bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if sprite.texture == null:
+		set_sprite_texture()
+	if collision_polygon.polygon.is_empty():
+		set_collision_to_sprite()
+		
+func _process(delta):
+	if has_mouse and !prop_taken and Input.is_action_just_pressed("lmb"):
+		set_taken(true)
+	elif prop_taken and Input.is_action_just_pressed("lmb"):
+		set_taken(false)
+		
+	if prop_taken:
+		var current_position : Vector2 = self.global_position
+		var mouse_position : Vector2 = get_global_mouse_position()
+		
+		var distance : float = current_position.distance_to(mouse_position)
+		var direction : Vector2 = current_position.direction_to(mouse_position)
+		
+		var speed : float = min(distance, MAX_SPEED)
+		
+		var velocity : Vector2 = direction * speed
+		move_and_collide(velocity)
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	var len = min(MAX_SPEED, state.linear_velocity.length())
+	state.linear_velocity = state.linear_velocity.normalized() * len
+	
+
+
+func _on_mouse_entered() -> void:
+	has_mouse = true
+
+
+func _on_mouse_exited() -> void:
+	has_mouse = false
+
+func set_sprite_texture() -> void:
 	sprite.texture = sprite_texture
+	
+func set_collision_to_sprite() -> void:
 	var bm: BitMap = BitMap.new()
 	bm.create_from_image_alpha(sprite.texture.get_image())
 	var rect: Rect2 = Rect2(0, 0, sprite.texture.get_width(), sprite.texture.get_height())
@@ -25,27 +66,10 @@ func _ready() -> void:
 	
 	# Place the polygon at sprite location
 	# We need to offset by width and height because sprite position is from the center, whereas the polygon position is from the top left
-	collision_polygon.position = sprite.position - Vector2(sprite.texture.get_width()/2, sprite.texture.get_height()/2)
-
-func _process(delta):
-	if has_mouse and !prop_taken and Input.is_action_just_pressed("lmb"):
-		set_taken(true)
-	elif prop_taken and Input.is_action_just_pressed("lmb"):
-		set_taken(false)
-		
-	if prop_taken:
-		var direction = global_position.direction_to(get_global_mouse_position())
-		linear_velocity = SPEED * direction
-		#move_and_collide()
-		global_position = global_position.lerp(get_global_mouse_position(), SPEED*delta)
-	
-
-func _on_mouse_entered() -> void:
-	has_mouse = true
+	collision_polygon.position = sprite.position - Vector2(sprite.texture.get_width()*sprite.scale.x/2, sprite.texture.get_height()*sprite.scale.y/2)
+	collision_polygon.scale = sprite.scale
 
 
-func _on_mouse_exited() -> void:
-	has_mouse = false
 
 func set_taken(taken: bool) -> void:
 	prop_taken = taken
--- scripts/static_prop.gd
+++ scripts/static_prop.gd
@@ -7,7 +7,15 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if sprite.texture == null:
+		set_sprite_texture()
+	if collision_polygon.polygon.is_empty():
+		set_collision_to_sprite()
+
+func set_sprite_texture() -> void:
 	sprite.texture = sprite_texture
+	
+func set_collision_to_sprite() -> void:
 	var bm: BitMap = BitMap.new()
 	bm.create_from_image_alpha(sprite.texture.get_image())
 	var rect: Rect2 = Rect2(0, 0, sprite.texture.get_width(), sprite.texture.get_height())
@@ -21,4 +29,5 @@ func _ready() -> void:
 	
 	# Place the polygon at sprite location
 	# We need to offset by width and height because sprite position is from the center, whereas the polygon position is from the top left
-	collision_polygon.position = sprite.position - Vector2(sprite.texture.get_width()/2, sprite.texture.get_height()/2)
+	collision_polygon.position = sprite.position - Vector2(sprite.texture.get_width()*sprite.scale.x/2, sprite.texture.get_height()*sprite.scale.y/2)
+	collision_polygon.scale = sprite.scale
